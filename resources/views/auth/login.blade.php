4836eb4dfdf9bf67d556fbc5aca7a3026e8cca78
Edwar2807 <edwar280700@gmail.com>
2024-01-10 16:37:27 -0500

    Login nuevo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,98 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card rounded">
 
-                <div class="card-body">
-                    <h2 class="text-center">REMOTE CONTROL LABORATORY</h2>
-                    <br>
-                    <form method="POST" action="{{ route('login') }}" id="login">
-                        @csrf
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form  method="POST" action="{{ route('login') }}" id="login">
+            @csrf
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+          </div>
+          <br>
+          <!-- Email input -->
+          <div class="col-md-6">
 
-                        <div class="form-group row">
-                            <label for="usuario" class="col-md-4 col-form-label text-md-right">User</label>
+        </div>
+          {{--  --}}
+          <div class="form-outline mb-4">
+            <input id="usuario" type="text" class=" form-control-lg form-control{{ $errors->has('usuario') ? ' is-invalid' : '' }}" name="usuario" value="{{ old('usuario') }}" required autofocus placeholder="Your Email Or Username">
+            @if ($errors->has('usuario'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('usuario') }}</strong>
+                </span>
+            @endif
+            <label class="form-label" for="form3Example3">Usuario</label>        
+        </div>
 
-                            <div class="col-md-6">
-                                <input id="usuario" type="text" class="form-control{{ $errors->has('usuario') ? ' is-invalid' : '' }}" name="usuario" value="{{ old('usuario') }}" required autofocus placeholder="Your Email Or Username">
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <input id="password" type="password" class="form-control-lg form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                                @if ($errors->has('usuario'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('usuario') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            @if ($errors->has('password'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+            <label class="form-label" for="form3Example4">Password</label>
+        </div>
+          {{--  --}}
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        Remember me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-12 text-center">
-                                <button type="submit" class="btn btn-success">
-                                    Login
-                                </button>
-                                
-                                @if (Route::has('password.request'))
-                                    <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        ¿Olvidó su contraseña?
-                                    </a> -->
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+          {{--  --}}
+          <div class="d-flex justify-content-between align-items-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+              <label class="form-check-label" for="remember">
+                Remember me
+            </label>
+            </div>
+          </div>
+          {{--  --}}
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <div class="col-12 text-center">
+                <button type="submit" class="btn btn-primary btn-lg"  style="padding-left: 2.5rem; padding-right: 2.5rem;">
+                    Login
+                </button>                
+                @if (Route::has('password.request'))
+                    <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                        ¿Olvidó su contraseña?
+                    </a> -->
+                @endif
             </div>
         </div>
+          {{--  --}}
+        </form>
+      </div>
     </div>
-</div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div
+    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    <!-- Copyright -->
+    <div class="text-white mb-3 mb-md-0">
+      Copyright © 202. All rights reserved.
+    </div>
+  </div>
+</section>
+
 @endsection
 @section('scripts')
 <script type="text/javascript">
